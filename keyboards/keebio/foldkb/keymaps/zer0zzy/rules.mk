e962fd408298c782935f58da0e2ce90be93878bf
zer0zzy <zer0zzy@pm.me>
2024-06-08 18:14:06 -0400

    Finished the foldkb config

--- keyboards/keebio/foldkb/keymaps/zer0zzy/rules.mk
+++ keyboards/keebio/foldkb/keymaps/zer0zzy/rules.mk
@@ -1,3 +1,13 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 ENCODER_MAP_ENABLE = yes
 NKRO_ENABLE = yes            # Enable N-Key Rollover
 RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+AUDIO_ENABLE = no
+AUTO_SHIFT_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+SPACE_CADET_ENABLE = no
+COMBO_ENABLE = yes
+LEADER_ENABLE = yes
+LAYER_LOCK_ENABLE = ye
+TRI_LAYER_ENABLE = yes
