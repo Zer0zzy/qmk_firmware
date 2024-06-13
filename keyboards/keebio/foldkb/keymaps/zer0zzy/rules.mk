9ed2db8fe12f732e30547fdc38ead1e803c7228e
zer0zzy <zer0zzy@pm.me>
2024-06-13 17:50:14 -0400

    Remove RGB Underglow from foldkb to save 5000 bytes.

--- keyboards/keebio/foldkb/keymaps/zer0zzy/rules.mk
+++ keyboards/keebio/foldkb/keymaps/zer0zzy/rules.mk
@@ -1,13 +1,17 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 ENCODER_MAP_ENABLE = yes
+MOUSEKEY_ENABLE = no
 NKRO_ENABLE = yes            # Enable N-Key Rollover
-RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no		 # Enable keyboard RGB underglow
 AUDIO_ENABLE = no
 AUTO_SHIFT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
 SPACE_CADET_ENABLE = no
 COMBO_ENABLE = yes
 LEADER_ENABLE = yes
-LAYER_LOCK_ENABLE = ye
 TRI_LAYER_ENABLE = yes
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no 
+MAGIC_ENABLE = no
