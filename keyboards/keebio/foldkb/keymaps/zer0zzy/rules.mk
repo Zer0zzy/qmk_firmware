64b21ab036057c1f06df2c89fe6345ca129d30ff
zer0zzy <zer0zzy@pm.me>
2024-06-13 18:00:48 -0400

    update foldkb with scrolling

--- keyboards/keebio/foldkb/keymaps/zer0zzy/rules.mk
+++ keyboards/keebio/foldkb/keymaps/zer0zzy/rules.mk
@@ -1,10 +1,10 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 ENCODER_MAP_ENABLE = yes
-MOUSEKEY_ENABLE = no
-NKRO_ENABLE = yes            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no        # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no		 # Enable keyboard RGB underglow
+MOUSEKEY_ENABLE = yes		# Disabling will free up 1400 bytes
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no		# Enable keyboard RGB underglow
 AUDIO_ENABLE = no
 AUTO_SHIFT_ENABLE = yes
 TAP_DANCE_ENABLE = yes
